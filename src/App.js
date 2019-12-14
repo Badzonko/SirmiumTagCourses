76326b3f8861b59e78e8650abfc4f11c66c33c42
Nikola Jeremic <jeremicnikola97@gmail.com>
2019-12-14 13:45:41 +0100

    Player imported, just style is remaining!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.videos = [
+      'https://www.youtube.com/watch?v=zTitoHKsyJg',
+      'https://www.youtube.com/watch?v=_xWljJScu70',
+      'https://www.youtube.com/watch?v=zTitoHKsyJg',
+      'https://www.youtube.com/watch?v=zTitoHKsyJg',
+      'https://www.youtube.com/watch?v=zTitoHKsyJg',
+      'https://www.youtube.com/watch?v=zTitoHKsyJg',
+    ]
+    this.state = {
+      currentIndex: 0,
+    }
+    this.goToPrevSlide = this.goToPrevSlide.bind(this);
+    this.goToNextSlide = this.goToNextSlide.bind(this);
+
+  }
+  goToPrevSlide () {
+    const {currentIndex} = this.state;
+    const newPointer = currentIndex === 0 ? this.videos.length -1 : currentIndex - 1;
+    this.setState({currentIndex: newPointer});
+  }
+  
+  goToNextSlide ()  {
+    const {currentIndex} = this.state;
+    const newPointer = currentIndex === this.videos.length - 1 ? 0 : currentIndex + 1;
+    this.setState({currentIndex: newPointer});
+  }
+  
+  render(){
+    return(
+        <div className="home">
+          <ReactPlayer url={this.videos[this.state.currentIndex]} controls={true}  />
+          <br/>
+          <button class = "prev" onClick={this.goToPrevSlide}>Previous lesson</button>
+          <button class = "next" onClick={this.goToNextSlide}>Next lesson</button>
+          <br/>
+          Index {this.state.currentIndex}
+        </div>
+
+    );
+}
+
 }
 
 export default App;
