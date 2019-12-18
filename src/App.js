d649f3de195bd56a2bc1d4c1507582ced3ff553c
Nikola Jeremic <jeremicnikola97@gmail.com>
2019-12-18 13:47:07 +0100

    Zavrsen je navigacioni bar i player

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import ReactPlayer from 'react-player';
 import Course from './Course';
+import Course1 from './Course1';
+import Course2 from './Course2';
+import Course3 from './Course3';
 import Navbar from './Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -19,6 +22,9 @@ class App extends React.Component {
         <Navbar/>
       <Switch>
           <Route path="/Course" component={Course}/>
+          <Route path="Course1" component={Course1}/>
+          <Route path="Course2" component={Course2}/>
+          <Route path="Course3" component={Course3}/>
       </Switch>
       </div>
       </Router>
--- src/Course.js
+++ src/Course.js
@@ -1,6 +1,8 @@
 import ReactPlayer from 'react-player';
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class Course extends React.Component {
   constructor(){
@@ -13,9 +15,18 @@ class Course extends React.Component {
       'https://www.youtube.com/watch?v=zTitoHKsyJg',
       'https://www.youtube.com/watch?v=zTitoHKsyJg',
     ]
+    this.title = [
+      'Naslov 1',
+      'Naslov 2',
+      'Naslov 3',
+      'Naslov 4',
+      'Naslov 5',
+      'Naslov 6',
+    ]
     this.state = {
       currentIndex: 0,
     }
+
     this.goToPrevSlide = this.goToPrevSlide.bind(this);
     this.goToNextSlide = this.goToNextSlide.bind(this);
 
@@ -24,28 +35,35 @@ class Course extends React.Component {
     const {currentIndex} = this.state;
     const newPointer = currentIndex === 0 ? this.videos.length -1 : currentIndex - 1;
     this.setState({currentIndex: newPointer});
+    const newPointerrr = currentIndex === 0 ? this.title.length -1 : currentIndex - 1;
+    this.setState({currentIndex: newPointerrr});
   }
+  
   
   goToNextSlide ()  {
     const {currentIndex} = this.state;
-    const newPointer = currentIndex === this.videos.length - 1 ? 0 : currentIndex + 1;
+    const newPointer = currentIndex === this.videos.length - 1  ? 0 : currentIndex + 1;
     this.setState({currentIndex: newPointer});
+    const newPointerr = currentIndex === this.title.length - 1  ? 0 : currentIndex + 1;
+    this.setState({currentIndex: newPointerr});
   }
+
   
   render(){
     return(
         <div className="home">
-
+          <h2 className="naslov">{this.title[this.state.currentIndex]}</h2>
           <div className="player">
           <div></div>
-          <ReactPlayer id="player" url={this.videos[this.state.currentIndex]} controls={true}  />
+          <ReactPlayer width='100%'
+           url={this.videos[this.state.currentIndex]} controls={true}  />
           <div ></div>
           </div>
           <br/>
           <div className="Buttons">
           <button className="invisible"></button>
-          <button  class = "prev" onClick={this.goToPrevSlide}>Previous lesson</button>
-          <button  class = "next" onClick={this.goToNextSlide}>Next lesson</button>
+          <button  disabled={this.state.currentIndex === 0} class = "prev "  onClick={this.goToPrevSlide}>Previous lesson</button>
+          <button disabled={this.state.currentIndex === 5} class = "next" onClick={this.goToNextSlide}>Next lesson</button>
           <button className="invisible"></button>
 
           </div>
@@ -53,6 +71,7 @@ class Course extends React.Component {
           
           <br/>
          
+
         </div>
 
     );
