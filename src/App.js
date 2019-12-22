cf9887d5f97950b8c43ac1a61d6c5d7110add263
Nikola Jeremic <jeremicnikola97@gmail.com>
2019-12-22 02:20:55 +0100

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
       <div className='wrapper'>
         <Navbar/>
       <Switch>
-        <Route path="/" component={Pocetna}/>
+        <Route path="/" component={Pocetna} exact/>
           <Route path="/Bootstrap" component={Bootstrap}/>
           <Route path="/Css" component={Css}/>
           <Route path="/Reactjs" component={Reactjs}/>
