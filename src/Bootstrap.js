3335b78b83698bff524fdf55433371c8b265f88a
Nikola Jeremic <jeremicnikola97@gmail.com>
2020-03-01 18:25:56 +0100

    Done

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -12,18 +12,18 @@ class Bootstrap extends React.Component {
   constructor(){
     super()
     this.videos = [
-      'https://www.youtube.com/watch?v=QAgrHLtG1Yk&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv',
-      'https://www.youtube.com/watch?v=N77ZsKq4esI&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=2',
-      'https://www.youtube.com/watch?v=4G4aBvRB3B4&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=3',
-      'https://www.youtube.com/watch?v=E7R23erzuGg&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=4',
-      'https://www.youtube.com/watch?v=CGpWqSIjCbQ&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=5',
-      'https://www.youtube.com/watch?v=rvlvpX-rbgc&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=6',
-      'https://www.youtube.com/watch?v=9Lpxzhzjy9w&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=7',
-      'https://www.youtube.com/watch?v=WWKuaUtgNBI&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=8',
-      'https://www.youtube.com/watch?v=Izc-0yF4EA0&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=9',
-      'https://www.youtube.com/watch?v=YRLOf5V1eHo&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=10',
-      'https://www.youtube.com/watch?v=g3iipbujyDg&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=11',
-      'https://www.youtube.com/watch?v=AeCFqEz5Whs&list=PL4cUxeGkcC9jE_cGvLLC60C_PeF_24pvv&index=12',
+      'https://youtu.be/QAgrHLtG1Yk',
+      'https://youtu.be/N77ZsKq4esI',
+      'https://youtu.be/4G4aBvRB3B4',
+      'https://youtu.be/E7R23erzuGg',
+      'https://youtu.be/CGpWqSIjCbQ',
+      'https://youtu.be/rvlvpX-rbgc',
+      'https://youtu.be/9Lpxzhzjy9w',
+      'https://youtu.be/WWKuaUtgNBI',
+      'https://youtu.be/Izc-0yF4EA0',
+      'https://youtu.be/YRLOf5V1eHo',
+      'https://youtu.be/g3iipbujyDg',
+      'https://youtu.be/AeCFqEz5Whs',
     ]
     this.title = [
       'Bootstrap 4 Tutorial #1 - Introduction',
