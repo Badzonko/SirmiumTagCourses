3648bba0e174e5260d134300834974760a0010cf
Nikola Jeremic <jeremicnikola97@gmail.com>
2020-03-09 04:04:08 -0700

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,8 @@ import './App.css';
 import gmail from './gmail.png';
 import dev from './dev.png'
 import github from './github.png';
+import 'bootstrap/dist/css/bootstrap.css';
+
 class Footer extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +14,7 @@ class Footer extends Component {
     render() { 
         return ( 
             <div>
-            <footer>
+            <footer className="page-footer">
 <div className="footerTabovi"><a target="_blank"href="mailto:badzo997@gmail.com"><img className="footerIkonice" src={gmail}/></a></div>
 <div className="footerTabovi"><a target="_blank" href="https://www.badzodev.com/"><img className="footerIkonice"  src={dev} /></a></div>
 <div className="footerTabovi"><a target="_blank" href="https://github.com/SirmiumTag"><img className="footerIkonice" src={github} /></a></div>
